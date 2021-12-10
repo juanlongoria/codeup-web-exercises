1733779f0d55a891c67df94d2b6b4a5141150675
Juan Longoria <juan.a.longoria66@gmail.com>
2021-12-10 12:37:03 -0600

    modified files and worked on exercises

--- js/promises.js
+++ js/promises.js
@@ -1,18 +1,58 @@
-function userName() {
-    fetch(url, {headers: {'Authorization': 'token'+GIT_HUB_KEY}})
+// function userName() {
+//     fetch(url, {headers: {'Authorization': 'token'+GIT_HUB_KEY}})
+//
+// }
 
+(() => {
+const userLastCommit = (user) => {
+    return fetch(`https://api.github.com/users/${user}/events`, {headers:{'Authorization': `token ${GIT_HUB_KEY}`}})
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data);
+            for (let event of data) {
+                if(event.type === "PushEvent") {
+                    console.log(new Date(event.created_at));
+                    break;
+                }
+                // console.log(1)
+        }
+});
 }
 
+userLastCommit("juanlongoria");
 
 
-setTimeout(()=> {
+//
+
+// const wait = ms => new Promise(resolve => {
+//     setTimeout(resolve, ms);
+// });
 
 
-    console.log('You\'ll see this after 1 second');
-}, 1000);
 
-setTimeout(()=> {
+const wait = ms => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(`You'll see this after ${ms/1000} second(s).`);
+        }, ms);
+    })
+}
+
+wait(1000).then(message => console.log(message));
+wait(3000).then(message => console.log(message));
+wait(5000).then(message => console.log(message));
+
+})();
 
 
-    console.log('You\'ll see this after 3 second');
-}, 3000);
+// setTimeout(()=> {
+//
+//
+//     console.log('You\'ll see this after 1 second');
+// }, 1000);
+//
+// setTimeout(()=> {
+//
+//
+//     console.log('You\'ll see this after 3 second');
+// }, 3000);
